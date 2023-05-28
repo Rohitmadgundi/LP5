05853ae3c6c1528e828c571e2ffeab9b43481651
Rohit <45898595+Rohitmadgundi@users.noreply.github.com>
2023-05-29 00:17:34 +0530

    Update parallel_bubble_sort.cpp

--- parallel_bubble_sort.cpp
+++ parallel_bubble_sort.cpp
@@ -2,36 +2,30 @@
 #include <vector>
 #include <omp.h>
 
-// Function to perform bubble sort on a given range of the vector
-void bubbleSortRange(std::vector<int>& arr, int start, int end) {
-    for (int i = start; i <= end; ++i) {
-        for (int j = start; j < end - i; ++j) {
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped = true;
+
+    for (int i = 0; i < n - 1 && swapped; i++) {
+        swapped = false;
+
+        // Even iteration
+#pragma omp parallel for shared(arr, swapped)
+        for (int j = 0; j < n - 1; j += 2) {
             if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
-    }
-}
 
-// Function to perform parallel bubble sort
-void parallelBubbleSort(std::vector<int>& arr) {
-    int size = arr.size();
-    bool sorted = false;
-
-    // Parallel bubble sort until the array is sorted
-    while (!sorted) {
-        sorted = true;
-
-        #pragma omp parallel for
-        for (int i = 0; i < size - 1; i += 2) {
-            bubbleSortRange(arr, i, i + 1);
-        }
-
-        #pragma omp parallel for
-        for (int i = 0; i < size - 1; ++i) {
-            // Check if sorting is still required
-            if (arr[i] > arr[i + 1]) {
-                sorted = false;
+        // Odd iteration
+#pragma omp parallel for shared(arr, swapped)
+        for (int j = 1; j < n - 1; j += 2) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
     }
@@ -39,22 +33,34 @@ void parallelBubbleSort(std::vector<int>& arr) {
 
 int main() {
     int n;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
 
-    std::vector<int> arr(n);
-    std::cout << "Enter the elements:\n";
-    for (int i = 0; i < n; ++i) {
-        std::cin >> arr[i];
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    parallelBubbleSort(arr);
+    bubbleSort(arr);
 
-    std::cout << "Sorted array: ";
-    for (int num : arr) {
-        std::cout << num << " ";
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
+
+// In this modified code, the bubble sort algorithm is divided into even and odd iterations. In each iteration, even-indexed elements are compared and swapped in parallel, and then odd-indexed elements are compared and swapped in parallel. The loop terminates if no swaps are made in any iteration.
+
+// The OpenMP parallel for directive is used for both the even and odd iterations, allowing multiple threads to work on different portions of the array simultaneously. The swapped flag is shared among the threads to track if any swap has occurred.
+
+// Compile the code using the -fopenmp flag, as mentioned before, to enable OpenMP support.
+
+// Note that the performance improvement from using even-odd iterations may vary depending on the input data and the number of available threads.
+
+
+
+
